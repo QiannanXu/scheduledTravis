0d4e02675fe66de46ed8b27e231edd78cadc59d1
QiannanXu <qnxu@thoughtworks.com>
2017-01-24 21:02:18 +0800

    Fix can not push repo issue

--- start.sh
+++ start.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 set -e
 
-basedir=$(dirname $0)
-echo "baseDir: ${basedir}"
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+TARGET_BRANCH=master
 
-if [ -n "$GITHUB_API_KEY" ]; then
-    git init
-    git -c user.name='travis' -c user.email='travis' pull https://QiannanXu:$GITHUB_API_KEY@github.com/QiannanXu/scheduledTravis master
+ruby init.rb
 
-    ruby init.rb
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-    git add -A
-    git status
-    git -c user.name='travis' -c user.email='travis' commit -m 'hourly cronjob backup `date`'
-    git push https://QiannanXu:$GITHUB_API_KEY@github.com/QiannanXu/scheduledTravis master
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes on this push; exiting."
+    exit 0
 fi
+
+git add .
+git commit -m "Daily cronjob update ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
